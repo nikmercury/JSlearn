b4e3b79c50cc46e9068224bcdaa9ad873e17f865
Nikita <nikmercury1996@yahoo.com>
2022-10-25 23:36:28 +0300

    final on 25/10

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
+function buttonClick() {
+  let lira = document.getElementById("TRY");
+  let dolar = document.getElementById("USD");
+  let ruble = document.getElementById("RUB");
+  let lari = document.getElementById("GEL");
 
-
-
-
-
-
-
-
-
-
-
-
-
+  dolar.value = (Number(lira.value) / 18.5).toFixed(1);
+  ruble.value = (dolar.value * 73).toFixed(0);
+  lari.value = (dolar.value * 3).toFixed(1);
+}
